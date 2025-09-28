5f5c4b6b234f778fb5df6f9597d301e9f5f0b5db
Muskan Goyal <goyalmuskan210@gmail.com>
2025-09-28 16:06:27 +0530

    resoved

--- frontend/interview-prep-ai/vite.config.js
+++ frontend/interview-prep-ai/vite.config.js
@@ -5,11 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ['@rollup/rollup-linux-x64-gnu'],
-    },
-  },
 })
 
 
